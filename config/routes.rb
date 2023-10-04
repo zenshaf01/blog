417cd54a9faeb0aa6e89f44aedb27d4a1bd68dc3
zenshaf01 <zeeshanshafqat1@outlook.com>
2023-10-04 12:33:54 +1100

    Our basic blog

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  devise_for :users
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+
+  resources :blog_posts
+
+  root "blog_posts#index"
+end
