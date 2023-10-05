fd0e4b35614df4ff5af371788427662f7a54ef53
zenshaf01 <zeeshanshafqat1@outlook.com>
2023-10-05 11:27:29 +1100

    Changed storage of images from local to s3 for production

--- app/models/blog_post.rb
+++ app/models/blog_post.rb
@@ -1,4 +1,6 @@
 class BlogPost < ApplicationRecord
+    has_rich_text :content
+
     validates :title, presence: true
-    validates :body, presence: true
+    validates :content, presence: true
 end
